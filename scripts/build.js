e9807a7c3a08db79a0d7a1ab0ea68487bed81e5a
Protinam <protinam@protonmail.ch>
2018-01-16 17:19:46 -0800

    Layout contd.

---
+++ scripts/build.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+
+if (!fs.existsSync('./build')) {
+  fs.mkdirSync('./build')
+}
+
+var schemas = fs.readdirSync('./schemas')
+schemas = schemas.map(name => {
+  const config = JSON.parse(fs.readFileSync('./schemas/' + name + '/config.json'))
+  return {
+    config: config
+  }
+})
+
+fs.writeFileSync('./build/schemas.json', JSON.stringify(schemas))
+fs.writeFileSync('./build/tokens.json', JSON.stringify(JSON.parse(fs.readFileSync('./tokens/tokens.json'))))
