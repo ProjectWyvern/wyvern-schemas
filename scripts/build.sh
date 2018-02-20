8cba77677ce748add3ed72ac916303b13fd8d907
Protinam <protinam@protonmail.ch>
2018-02-19 16:15:54 -0800

    Transpile JS

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,5 +3,7 @@
 echo 'Generating token list from tokenbase...'
 node scripts/gen_from_tokenbase.js
 echo 'Compiling Typescript...'
-rm -rf dist
+rm -rf dist-tsc dist
 yarn run tsc
+echo 'Transpiling with Babel...'
+yarn run babel dist-tsc --out-dir dist --source-maps
