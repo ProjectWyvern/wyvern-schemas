6adabb3c9b30814bd31d587d4f8a96dc0382ccc5
Alexander Atallah <alexander.atallah@gmail.com>
2019-07-02 17:00:44 -0700

    add json metadata to tsc dist build

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,5 +9,6 @@ echo 'Transpiling with Babel...'
 yarn run babel dist-tsc --out-dir dist --source-maps
 for DIR in $(ls src/schemas/main); do
   cp -r src/schemas/main/$DIR/*.json dist/schemas/main/$DIR
+  cp -r src/schemas/main/$DIR/*.json dist-tsc/schemas/main/$DIR
 done
 echo 'Done'
