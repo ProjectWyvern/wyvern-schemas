21a27e758aedbabc19cd7d2ea3452b50cc8d58eb
Joshua Tsubaki Wu <wujoshuawu@gmail.com>
2019-08-02 18:34:03 +0900

    Add rinkebyENSShortNameAuction schema.

---
+++ src/schemas/rinkeby/rinkebyENSShortNameAuction/index.ts
@@ -0,0 +1,81 @@
+import { AbiType } from 'web3';
+
+import {
+  EventInputKind,
+  FunctionInputKind,
+  Schema,
+  StateMutability,
+} from '../../../types';
+
+export type ENSShortName = string;
+
+export const RINKEBY_ENS_SHORT_NAME_AUCTION_ADDRESS =
+  '0x266ebd3ca5b8c472da228df3f7ab2cc951b19ec3';
+
+export const rinkebyENSShortNameAuctionSchema: Schema<ENSShortName> = {
+  version: 0,
+  deploymentBlock: 4791629,
+  name: 'ENSShortNameAuction',
+  description: 'ERC721 ENS short (3-6 character) names sold via auction.',
+  thumbnail: '', // TODO: put SVG body directly here or host a PNG ourselves?
+  website: 'https://ens.domains/',
+  fields: [{ name: 'Name', type: 'string', description: 'ENS short name' }],
+  assetFromFields: (fields: any) => fields.name,
+  assetToFields: asset => ({ name: asset }),
+  formatter: async asset => {
+    return {
+      title: 'ENS Short Name: ' + asset,
+      description: '',
+      url: '',
+      thumbnail: '',
+      properties: [],
+    };
+  },
+  functions: {
+    transfer: asset => ({
+      type: AbiType.Function,
+      name: 'register',
+      payable: false,
+      constant: false,
+      stateMutability: StateMutability.Nonpayable,
+      target: RINKEBY_ENS_SHORT_NAME_AUCTION_ADDRESS,
+      inputs: [
+        {
+          kind: FunctionInputKind.Data,
+          name: 'name',
+          type: 'string',
+          value: asset,
+        },
+        { kind: FunctionInputKind.Replaceable, name: 'owner', type: 'address' },
+      ],
+      outputs: [],
+    }),
+    assetsOfOwnerByIndex: [],
+  },
+  events: {
+    transfer: [
+      {
+        type: AbiType.Event,
+        name: 'NameRegistered',
+        target: RINKEBY_ENS_SHORT_NAME_AUCTION_ADDRESS,
+        anonymous: false,
+        inputs: [
+          {
+            kind: EventInputKind.Asset,
+            indexed: false,
+            name: 'name',
+            type: 'string',
+          },
+          {
+            kind: EventInputKind.Destination,
+            indexed: false,
+            name: 'owner',
+            type: 'address',
+          },
+        ],
+        assetFromInputs: async (inputs: any) => inputs.name,
+      },
+    ],
+  },
+  hash: asset => asset,
+};
