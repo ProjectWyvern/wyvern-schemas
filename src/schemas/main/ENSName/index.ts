513a78b4a21c67bc485fdbae0e1599770d6de2c3
Joshua Tsubaki Wu <wujoshuawu@gmail.com>
2019-08-02 22:21:59 +0900

    Autoformat main/ENSName/index.ts.

--- src/schemas/main/ENSName/index.ts
+++ src/schemas/main/ENSName/index.ts
@@ -1,15 +1,12 @@
 import * as Web3 from 'web3';
 
+import { ENSName, ENSNameBaseSchema } from '../../../common/ens';
 import {
   FunctionInputKind,
   FunctionOutputKind,
   Schema,
   StateMutability,
 } from '../../../types';
-import {
-  ENSName,
-  ENSNameBaseSchema,
-} from '../../../common/ens';
 
 export const ENSNameSchema: Schema<ENSName> = {
   ...ENSNameBaseSchema,
@@ -19,15 +16,16 @@ export const ENSNameSchema: Schema<ENSName> = {
   description: 'Ethereum Name Service Name (EIP 137)',
   thumbnail: 'https://ens.domains/img/ens.svg',
   website: 'https://github.com/ethereum/EIPs/blob/master/EIPS/eip-137.md',
-  formatter:
-    async asset => {
-      return {
-        thumbnail: 'https://ens.domains/img/ens.svg',
-        title: 'ENS Name ' + (asset.name ? asset.name : asset.nodeHash.slice(0, 4) + '...'),
-        description: 'ENS node ' + asset.nodeHash,
-        url: 'https://etherscan.io/enslookup?q=' + asset.name,
-        properties: [],
-      };
+  formatter: async asset => {
+    return {
+      thumbnail: 'https://ens.domains/img/ens.svg',
+      title:
+        'ENS Name ' +
+        (asset.name ? asset.name : asset.nodeHash.slice(0, 4) + '...'),
+      description: 'ENS node ' + asset.nodeHash,
+      url: 'https://etherscan.io/enslookup?q=' + asset.name,
+      properties: [],
+    };
   },
   functions: {
     transfer: asset => ({
@@ -38,8 +36,17 @@ export const ENSNameSchema: Schema<ENSName> = {
       stateMutability: StateMutability.Nonpayable,
       target: '0x6090a6e47849629b7245dfa1ca21d94cd15878ef',
       inputs: [
-        {kind: FunctionInputKind.Asset, name: '_hash', type: 'bytes32', value: asset.nodeHash},
-        {kind: FunctionInputKind.Replaceable, name: 'newOwner', type: 'address'},
+        {
+          kind: FunctionInputKind.Asset,
+          name: '_hash',
+          type: 'bytes32',
+          value: asset.nodeHash,
+        },
+        {
+          kind: FunctionInputKind.Replaceable,
+          name: 'newOwner',
+          type: 'address',
+        },
       ],
       outputs: [],
     }),
@@ -51,11 +58,14 @@ export const ENSNameSchema: Schema<ENSName> = {
       stateMutability: StateMutability.View,
       target: '0x314159265dD8dbb310642f98f50C066173C1259b',
       inputs: [
-        {kind: FunctionInputKind.Asset, name: 'node', type: 'bytes32', value: asset.nameHash},
+        {
+          kind: FunctionInputKind.Asset,
+          name: 'node',
+          type: 'bytes32',
+          value: asset.nameHash,
+        },
       ],
-      outputs: [
-        {kind: FunctionOutputKind.Owner, name: '', type: 'address'},
-      ],
+      outputs: [{ kind: FunctionOutputKind.Owner, name: '', type: 'address' }],
     }),
     assetsOfOwnerByIndex: [],
   },
